f502212e36c1964c57c0384f1c31b932cd65bfc9
Edith Mandujano <edithmandujano001@gmail.com>
2017-05-09 16:08:38 -0500

    borra pandas y texto

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,7 @@
 var cerrar = document.getElementsByClassName('cerrar');
+var ocultarIzq = document.getElementById('ocultaTextIzquierda');
+var ocultarDer = document.getElementById('ocultaTextDerecha');
+var restaurar = document.getElementById('restaurar');
 
 for (var i = 0; i < cerrar.length; i++) {
   cerrar[i].addEventListener('click', ocultarImagen);
@@ -7,4 +10,16 @@ for (var i = 0; i < cerrar.length; i++) {
 function ocultarImagen() {
   var padre = this.parentElement;
   padre.style.display = 'none';
+};
+
+ocultarIzq.addEventListener('click', ocultarIzqierda);
+function ocultarIzqierda() {
+  var textoIzquierda = document.getElementById('textoIzquierda');
+  textoIzquierda.style.display = 'none';
+};
+
+ocultarDer.addEventListener('click', ocultarDerecha);
+function ocultarDerecha() {
+  var textoDerecha = document.getElementById('textoDerecha');
+  textoDerecha.style.display = 'none';
 };
