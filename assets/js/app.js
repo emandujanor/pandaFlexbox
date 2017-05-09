0572a2b0cb34f6c4b7ceb31e73d6d15064a2f515
Edith Mandujano <edithmandujano001@gmail.com>
2017-05-09 16:22:21 -0500

    con funcionalidad

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,17 +9,33 @@ for (var i = 0; i < cerrar.length; i++) {
 
 function ocultarImagen() {
   var padre = this.parentElement;
-  padre.style.display = 'none';
+  padre.style.visibility = 'hidden';
 };
 
 ocultarIzq.addEventListener('click', ocultarIzqierda);
 function ocultarIzqierda() {
   var textoIzquierda = document.getElementById('textoIzquierda');
-  textoIzquierda.style.display = 'none';
+  textoIzquierda.style.visibility = 'hidden';
 };
 
 ocultarDer.addEventListener('click', ocultarDerecha);
 function ocultarDerecha() {
   var textoDerecha = document.getElementById('textoDerecha');
-  textoDerecha.style.display = 'none';
+  textoDerecha.style.visibility = 'hidden';
+};
+
+restaurar.addEventListener('click', mostrarTodo);
+function mostrarTodo() {
+  var textoDerecha = document.getElementById('textoDerecha');
+  var textoIzquierda = document.getElementById('textoIzquierda');
+  var pandaUno = document.getElementById('pandaUno');
+  var pandaDos = document.getElementById('pandaDos');
+  var pandaTres = document.getElementById('pandaTres');
+  var pandaCuatro = document.getElementById('pandaCuatro');
+  textoDerecha.style.visibility = 'visible';
+  textoIzquierda.style.visibility = 'visible';
+  pandaUno.style.visibility = 'visible';
+  pandaDos.style.visibility = 'visible';
+  pandaTres.style.visibility = 'visible';
+  pandaCuatro.style.visibility = 'visible';
 };
